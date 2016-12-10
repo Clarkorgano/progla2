dc8b78840554a83a1c6a2e893b2833c302fcc2fc
Clark Organo <clark.organo@yahoo.com>
2016-12-10 18:46:08 +0800

    Update edpogi.cpp

--- edpogi.cpp
+++ edpogi.cpp
@@ -1,24 +1,28 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main()
-{	
-for (int a=1;a<=100;a++)
+int main ()
 {
-	if (a%3==0)
+	for (int c=1 ; c<=100 ; c++)
 	{
-		cout <<"Pogi"<<endl;
+		if (c%3 == 0 && c%5 == 0)
+		{
+			cout << "Ed Pogi" << endl;
+		}
+		else if (c%5 == 0)
+		{
+			cout << "Ed" << endl;
+		}
+		else if (c%3 == 0)
+		{
+			cout << "Pogi" << endl;
+		}
+		else
+		{
+			cout << c << endl;
+		}
 	}
-	else if (a%5==0)
-	{
-		cout<< "Ed"<<endl;
-	}
-	else
-	{
 	
-	
-	cout <<a<<endl;
-}
-}
-	return 0;	
+	return 0;
 }
