d03b53a85d91a9161d337dbc2fce0f1ff82f3ca5
Clark Organo <clark.organo@yahoo.com>
2016-12-10 18:40:50 +0800

    Create edpogi.cpp

---
+++ edpogi.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{	
+for (int a=1;a<=100;a++)
+{
+	if (a%3==0)
+	{
+		cout <<"Pogi"<<endl;
+	}
+	else if (a%5==0)
+	{
+		cout<< "Ed"<<endl;
+	}
+	else
+	{
+	
+	
+	cout <<a<<endl;
+}
+}
+	return 0;	
+}
